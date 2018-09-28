e6703d39b7874ae6236bb97ce48d2f3201894f31
RMunhoz <rogerio.munhoz@hotmail.com.br>
2018-09-28 15:49:24 -0300

    admin login

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group([
-    'prefix' => 'admin',
-    'as' => 'admin',
-    'middleware' => 'can:admin'
+    'prefix'        => 'admin',
+    'as'            => 'admin.',
+    'namespace'     => 'Admin\\'
 ], function (){
-    Route::get('/', function (){
-        return "Area Administrativa";
+    Route::name('login')->get('login', 'Auth\LoginController@showLoginForm');
+    Route::post('login', 'Auth\LoginController@login');
+
+    Route::group(['middleware' => 'can:admin'], function (){
+        Route::name('logout')->post('logout', 'Auth\LoginController@logout');
+        Route::get('dashboard', function (){
+           return "Area administrativa funcionando";
+        });
     });
+
 });
